55ea8f984c8770786d2721e0c6c6d7856f34353f
Kushagra Mehrotra <68564934+kushaagr@users.noreply.github.com>
2023-07-20 20:23:13 +0530

    Show pubic IP instead of private IP

--- receiver.py
+++ receiver.py
@@ -1,18 +1,27 @@
 import socket
 import threading
 import os
+import requests
 import keyboard as kb
 import datetime as dt
 
 import debug_utils
 from debug_utils import debug
 
-debug_utils.dev_mode = True
+# debug_utils.dev_mode = True
 
 HOSTNAME = socket.gethostname()
 HOST = socket.gethostbyname(HOSTNAME)
-PORT = 80
+PORT = 8181
 CONNECTIONS_LIMIT = 1 # ONE-TO-ONE CONNECTION
+
+
+def get_public_ip() -> str:
+    """ A utility function to retrieve your public IPv4 address from
+    checkip.amazonaws.com
+    """
+    url = 'https://checkip.amazonaws.com'
+    return requests.get(url).text.strip()
 
 
 def create_dir(directory: str) -> str:
@@ -46,28 +55,37 @@ def client_handler(client: socket.socket) -> None:
             ).replace(':','').replace('.', '') + '.png'
             filepath = os.path.join(images_path, newfile)
             file = open(filepath, 'ab')
+
+            print(f"Received! Saving file '{newfile}' at {images_path}")
             debug("START TRANSFER")
+            
         # elif data == b'end':
         #     debug("Received file end signal.")
         #     debug("END TRANSFER")
         #     if file and not file.closed: 
         #         # file.close()
         #         print(f"{newfile} saved at {images_path}")
-        else:
+
+        elif hasattr(file, 'write'):
             file.write(data)
             # debug("IN PROCESS", end="--")
-            debug("[{0}]".format(data[-6:]), end="--")
+            # debug("[{0}]".format(data[-6:]), end="--")
 
-
+debug("Fetching public IPv4 address..")
+PUBLIC_HOST = get_public_ip()
 print("Starting server...")
 debug("dev_mode ON: DEBUG messages will be printed!")
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOSTNAME, PORT))
+server.listen(CONNECTIONS_LIMIT)
+print()
+debug(f"Your private ip is: {HOST}")
+debug(f"Your public ip is: {PUBLIC_HOST}")
+print(f"Server listening on {PUBLIC_HOST} : {PORT} (Press Ctrl+Pause to stop.)")
+
 while True:
     try:
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.bind((HOSTNAME, PORT))
-        server.listen(CONNECTIONS_LIMIT)
-        print()
-        print(f"Server listening on {HOST} : {PORT} (Press Ctrl+Pause to stop.)")
         client, c_addr = server.accept()
         print(f"Connected to client on {c_addr}")
         # print("Available methods on client: ", dir(client))
